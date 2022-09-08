04120281faff730df3e2c6d2cbe9719fbfdb5b61
MYZT3RY <d1maz.d@yandex.ru>
2022-09-09 00:06:05 +0300

    Issue #34

--- messages/counter.py
+++ messages/counter.py
@@ -26,29 +26,107 @@ def messageCounter(message):
         cursor.execute("insert into`{0}`(`userid`,`username`,`chatid`)values('{1}','{2}','{3}')".format(dbConfig.tblUsers,tgUserId,tgUsername,tgChatId))
     else: # else updating info
         if message.text != None or len(message.entities) != 0: # Text messages
-            cursor.execute("update`{0}`set`messages`=`messages`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+	                            , c.messages = c.messages + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.voice != None: # Audio voice messages
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`voices`=`voices`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`voices`=`voices`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.voices = u.voices + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.voices = c.voices + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.video != None: # Video files
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`videos`=`videos`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`videos`=`videos`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.videos = u.videos + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.videos = c.videos + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.audio != None: # Audio files
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`audios`=`audios`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`audios`=`audios`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.audios = u.audios + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.audios = c.audios + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif len(message.photo) != 0: # Photo files
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`photos`=`photos`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`photos`=`photos`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.photos = u.photos + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.photos = c.photos + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.document != None: # Documents
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`documents`=`documents`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`documents`=`documents`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.documents = u.documents + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.documents = c.documents + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.sticker != None: # Stickers
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`stickers`=`stickers`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`stickers`=`stickers`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.stickers = u.stickers + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.stickers = c.stickers + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
         elif message.video_note != None: # Video messages
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`videovoices`=`videovoices`+'1'where`userid`='{1}'and`chatid`='{2}'".format(dbConfig.tblUsers,tgUserId,tgChatId))
-            cursor.execute("update`{0}`set`messages`=`messages`+'1',`videovoices`=`videovoices`+'1'where`id`='{1}'".format(dbConfig.tblChats,tgChatId))
+            cursor.execute("update \
+	                            {0} u \
+		                            left join {1} c on c.id = u.chatid \
+                            set \
+	                            u.messages = u.messages + '1' \
+                                , u.videovoices = u.videovoices + '1' \
+	                            , c.messages = c.messages + '1' \
+                                , c.videovoices = c.videovoices + '1' \
+                            where \
+	                            u.userid = '{2}' \
+	                            and c.id = '{3}' \
+            ".format(dbConfig.tblUsers, dbConfig.tblChats, tgUserId, tgChatId))
 
     # closing database connection
     db.commit()
