e9fe8759bd45ffb82618538025a64c13a0dfdd19
MYZT3RY <d1maz.d@yandex.ru>
2023-02-09 02:40:36 +0300

    Fixes for #31 & #32

--- commands/anek.py
+++ commands/anek.py
@@ -6,6 +6,8 @@ from telegram.constants import PARSEMODE_HTML
 import requests
 from bs4 import BeautifulSoup
 from configs import config
+from configs import dbConfig
+from db import dbconnect
 
 def cmdAnek(update: Update, context: CallbackContext):
     counter.messageCounter(update.message)
@@ -20,14 +22,17 @@ def cmdAnek(update: Update, context: CallbackContext):
 
     randPhoto = random.randint(1, config.valueOfAnekBg)
     photoPath = open("{0}{1}{2:02}.jpg".format(config.pathToAnekBg, config.prefixAnekBg, randPhoto), 'rb')
-
-    key = 'settings' + str(tgChatId)
-
-    anekBg = context.chat_data[key]["anekbg"]
     
     string = "\n<i><b>{0}</b></i>\n\n{1}\n".format(numberOfAnek, result)
 
-    if anekBg is True:
+    db = dbconnect.dbConnect()
+    cursor = db.cursor()
+
+    cursor.execute("select `show_anek_bg` from `{0}` where `chatid` = '{1}'".format(dbConfig.tblChatSettings, tgChatId))
+    rows = cursor.fetchall()
+    row = rows[0]
+
+    if row["show_anek_bg"] == 1:
         context.bot.send_photo(chat_id=tgChatId, photo=photoPath, caption=string, parse_mode=PARSEMODE_HTML, disable_notification=True)
     else:
         context.bot.send_message(chat_id=tgChatId, text=string, parse_mode=PARSEMODE_HTML, disable_notification=True)
--- commands/settings.py
+++ commands/settings.py
@@ -1,11 +1,9 @@
-from ast import arg
 from telegram import Update
 from telegram.ext import CallbackContext
 from messages import counter
 from db import dbconnect
 from configs import dbConfig
 from telegram.constants import PARSEMODE_HTML
-from uuid import uuid4
 
 def cmdSettings(update: Update, context: CallbackContext):
     counter.messageCounter(update.message)
@@ -13,34 +11,30 @@ def cmdSettings(update: Update, context: CallbackContext):
     tgChatId = update.message.chat_id
     args = context.args
 
-    key = 'settings' + str(tgChatId)
-
-    if not (key in context.chat_data):
-        context.chat_data[key] = {'anekbg': True, 'test':'ТЫ ХУЙ'}
-
-    anekBg = context.chat_data[key]["anekbg"]
-
     db = dbconnect.dbConnect()
     cursor = db.cursor()
 
-    string = ''
+    cursor.execute("select * from `{0}` where `chatid` = '{1}'".format(dbConfig.tblChatSettings, tgChatId))
+    rows = cursor.fetchall()
+    row = rows[0]
+
+    string = None
 
     if len(args) < 1:
-        anekBg_string = 'включено' if anekBg is True else 'выключено'
+        anekBg_string = 'включено' if row["show_anek_bg"] == 1 else 'выключено'
 
-        string = "<b>Значения настроек чата</b>\n\nОтображение картинок в анекдотах (anekbg) - <b>{0}</b>".format(anekBg_string)
+        string = "<b>Значения настроек чата</b>\n\nОтображение картинок в анекдотах (anekbg) - <b>{0}</b>\n\nЧтобы изменить параметр, введите команду и аргумент.".format(anekBg_string)
     elif args[0] == 'anekbg':
-        anekBg = not anekBg
+        row["show_anek_bg"] = not row["show_anek_bg"]
 
-        context.chat_data[key]["anekbg"] = anekBg
+        cursor.execute("update `{0}` set `show_anek_bg` = '{1}' where `chatid` = '{2}'".format(dbConfig.tblChatSettings, int(row["show_anek_bg"]), tgChatId))
 
-        cursor.execute("update`chat_settings`set`show_anek_bg`='{0}'where`chatid`='{1}'".format(anekBg, tgChatId))
-
-        anekBg_string = 'включено' if anekBg is True else 'выключено'
+        anekBg_string = 'включено' if row["show_anek_bg"] == 1 else 'выключено'
 
         string = 'Отображение картинок в анекдотах <b>{0}</b>.'.format(anekBg_string)
     
     db.commit()
     db.close()
 
-    context.bot.send_message(chat_id=tgChatId, text=string, parse_mode=PARSEMODE_HTML, disable_notification=True)
+    if string is not None:
+        context.bot.send_message(chat_id=tgChatId, text=string, parse_mode=PARSEMODE_HTML, disable_notification=True)
