ac0f1a23bfe8ab67573ca2910353dbdee2ad4e94
MYZT3RY <d1maz.d@yandex.ru>
2023-02-09 03:50:06 +0300

    Features for #35

--- commands/updates.py
+++ commands/updates.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from messages import counter
 from db import dbconnect
+from configs import dbConfig
 from telegram.constants import PARSEMODE_HTML
 
 def cmdUpdates(update: Update, context: CallbackContext):
@@ -12,13 +13,24 @@ def cmdUpdates(update: Update, context: CallbackContext):
     db = dbconnect.dbConnect()
     cursor = db.cursor()
 
-    cursor.execute("select`text`from`updates`order by`id`desc limit 5")
+    cursor.execute("\
+        select \
+            `text` \
+            , date_format(create_date, '%\d.%\m.%\Y') as date \
+        from \
+            `{0}` \
+        order by \
+            `id` desc \
+        limit 5 \
+    ".format(dbConfig.tblUpdates))
     rows = cursor.fetchall()
     
     string = ""
 
     for row in rows:
-        tmp = "{0}\n\n".format(row["text"])
-        string = string + tmp
+        text = str(row["text"]).split("</b>")
+        result = text[0] + " - " + row["date"] + "</b>" + text[1]
+
+        string = string + "{0}\n\n".format(result)
 
     context.bot.send_message(chat_id=tgChatId, text=string, parse_mode=PARSEMODE_HTML, disable_notification=True)
